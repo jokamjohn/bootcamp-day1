91264908f45ce84c4569750c1aa3b2d7aee50c56
John Kagga <johnkagga@gmail.com>
2017-07-03 13:50:25 +0300

    Test an empty list is returned when the argument is negative and also that an integer list is returned

--- prime_number_test.py
+++ prime_number_test.py
@@ -3,10 +3,15 @@ from prime_number import prime_number
 
 
 class TestForPrimeNumbers(unittest.TestCase):
-    def test_raises_error_when_an_is_a_string(self):
+    def test_raises_error_when_arg_is_a_string(self):
         self.assertRaises(ValueError, prime_number, "2")
 
     def test_function_returns_a_list(self):
         self.assertEqual(type(list), prime_number(10), "A list is not returned")
 
+    def test_empty_list_returned_when_arg_is_negative(self):
+        self.assertEqual([], prime_number(-5), "Function should return an empty list")
 
+    def test_function_returns_list_of_integers(self):
+        self.assertEqual(all(isinstance(int, n) for n in prime_number(10)),
+                         "Returned list has non-integer elements")
